23dc06deb81143f8604008abf3ccdf72b718dc21
heroboy <yangweiqin@gmail.com>
2017-12-04 22:55:56 +0800

    fix https://github.com/softcup/New-Tongwentang-for-Firefox/issues/21 (#32)
    * test
    
    * fixifx
    
    * delay load script

--- src/tongwen.js
+++ src/tongwen.js
@@ -30,89 +30,95 @@ const docLoadedInit = uri => {
   }
   return zhflag;
 };
+function init()
+{
+  return browser.storage.local.get().then(results => {
+    if ((typeof results.length === 'number') && (results.length > 0)) {
+      results = results[0];
+    }
+    if (results.version) {
+      preferences = results;
+      //console.log(JSON.stringify(preferences, null, 4));
+      let zhflag = docLoadedInit(document.URL);
+      //console.log('zhflag = ' + zhflag );
+      TongWen.loadSettingData(preferences);
+      if (zhflag === 'trad') {
+        TongWen.trans2Trad(document);
+      }
+      else if (zhflag === 'simp') {
+        TongWen.trans2Simp(document);
+      }
+    }
+  });
+}
 
-browser.storage.local.get().then(results => {
-  if ((typeof results.length === 'number') && (results.length > 0)) {
-    results = results[0];
-  }
-  if (results.version) {
-    preferences = results;
-    //console.log(JSON.stringify(preferences, null, 4));
-    let zhflag = docLoadedInit(document.URL);
-    //console.log('zhflag = ' + zhflag );
-    TongWen.loadSettingData(preferences);
-    if (zhflag === 'trad') {
-      TongWen.trans2Trad(document);
-    }
-    else if (zhflag === 'simp') {
-      TongWen.trans2Simp(document);
-    }
-  }
-});
 
 const messageHandler = (request, sender, sendResponse) => {
-  //console.log(JSON.stringify(request, null , 4));
-  var isInput, val, tag, attr, zhflag, elem, lang;
-  if (request.act === 'paste') {
-    if(window.self === window.top) { //this message only handle by top window.
-      let val = TongWen.convert(request.text, request.flag);
-      let textArea = document.createElement('textarea');
-      textArea.value = val;
-      document.body.appendChild(textArea);
-      textArea.select();
-      document.execCommand('copy');
-      document.body.removeChild(textArea);
-      sendResponse({text: val});
-    }
-    return;
-  }
-
-  lang = document.documentElement.getAttribute('lang');
-  if ((lang === null) && (request.lang !== false)) {
-    document.documentElement.setAttribute('lang', request.lang);
-  }
-
-  elem = document.activeElement;
-  tag = (typeof elem.tagName === 'undefined') ? '' : elem.tagName.toLowerCase();
-  val = (typeof elem.type === 'undefined') ? '' : elem.type.toLowerCase();
-  isInput = ((['textarea', 'input'].indexOf(tag) >= 0) && (['textarea', 'text'].indexOf(val) >= 0));
-
-  if (isInput && ((request.act === 'input') || (convertMapping[preferences.inputConvert] !== 'none'))) {
-    // 輸入區文字轉換
-    zhflag = request.flag;
-    val = document.activeElement.value;
-    if (zhflag === 'auto') {
-      attr = document.activeElement.getAttribute('zhtongwen');
-      if (attr === null) {
-        zhflag = 'traditional';
+  init().then(function(){
+    var isInput, val, tag, attr, zhflag, elem, lang;
+    if (request.act === 'paste') {
+      if(window.self === window.top) { //this message only handle by top window.
+        let val = TongWen.convert(request.text, request.flag);
+        let textArea = document.createElement('textarea');
+        textArea.value = val;
+        document.body.appendChild(textArea);
+        textArea.select();
+        document.execCommand('copy');
+        document.body.removeChild(textArea);
+        sendResponse({text: val});
       }
-      else {
-        zhflag = (attr === 'traditional') ? 'simplified' : 'traditional';
+      return;
+    }
+  
+    lang = document.documentElement.getAttribute('lang');
+    if ((lang === null) && (request.lang !== false)) {
+      if (document.documentElement.hasAttribute('lang')){
+        document.documentElement.setAttribute('lang', request.lang);
       }
-      document.activeElement.setAttribute('zhtongwen', zhflag);
-      document.activeElement.value = TongWen.convert(val, zhflag);
     }
-    else if (zhflag === 'trad') {
-      document.activeElement.value = TongWen.convert(val, 'traditional');
+  
+    elem = document.activeElement;
+    tag = (typeof elem.tagName === 'undefined') ? '' : elem.tagName.toLowerCase();
+    val = (typeof elem.type === 'undefined') ? '' : elem.type.toLowerCase();
+    isInput = ((['textarea', 'input'].indexOf(tag) >= 0) && (['textarea', 'text'].indexOf(val) >= 0));
+  
+    if (isInput && ((request.act === 'input') || (convertMapping[preferences.inputConvert] !== 'none'))) {
+      // 輸入區文字轉換
+      zhflag = request.flag;
+      val = document.activeElement.value;
+      if (zhflag === 'auto') {
+        attr = document.activeElement.getAttribute('zhtongwen');
+        if (attr === null) {
+          zhflag = 'traditional';
+        }
+        else {
+          zhflag = (attr === 'traditional') ? 'simplified' : 'traditional';
+        }
+        document.activeElement.setAttribute('zhtongwen', zhflag);
+        document.activeElement.value = TongWen.convert(val, zhflag);
+      }
+      else if (zhflag === 'trad') {
+        document.activeElement.value = TongWen.convert(val, 'traditional');
+      }
+      else if (zhflag === 'simp') {
+        document.activeElement.value = TongWen.convert(val, 'simplified');
+      }
     }
-    else if (zhflag === 'simp') {
-      document.activeElement.value = TongWen.convert(val, 'simplified');
+    else {
+      // 網頁轉換
+      switch (request.flag) {
+        case 'auto':
+          TongWen.transAuto(document);
+          break;
+        case 'trad':
+          TongWen.trans2Trad(document);
+          break;
+        case 'simp':
+          TongWen.trans2Simp(document);
+          break;
+      }
     }
-  }
-  else {
-    // 網頁轉換
-    switch (request.flag) {
-      case 'auto':
-        TongWen.transAuto(document);
-        break;
-      case 'trad':
-        TongWen.trans2Trad(document);
-        break;
-      case 'simp':
-        TongWen.trans2Simp(document);
-        break;
-    }
-  }
+  });
 };
 
 browser.runtime.onMessage.addListener(messageHandler);
